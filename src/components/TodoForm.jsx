8ef945b4f7fc95a9aa23b4113f7cffbfa076c326
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-06-29 09:08:28 +0500

    Refactor TodoForm component for improved readability and consistency

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,24 +2,22 @@ import { useState } from "react";
 import { useTodo } from "../contexts/TodoContext";
 
 function TodoForm() {
+   const [todo , setTodo] = useState("");
+   const {addTodo} = useTodo();
 
-    const [todo , setTodo] = useState('')
-    const {addTodo} = useTodo()
+   const add = (e) => {
+     e.preventDefault()
 
-    const add  = (e) => {
-           e.preventDefault()
-           if(!todo) return 
-           addTodo({ todo , completed : false })
-           setTodo('')
-    }
+     if(!todo) return 
+     addTodo({todo , completed : false});
+     setTodo("")
+   }
     
 
     return (
-        <form onSubmit={add}  className="flex">
+        <form onSubmit={add} className="flex">
             <input
                 type="text"
-                value={todo}
-                onChange={(e) => setTodo(e.target.value)}
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 py-1.5"
             />
